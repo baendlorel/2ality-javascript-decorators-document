0bd7f9cca546197599915eaf3131c65072c95ed1
aldia_mechdream <futami16237@gmail.com>
2025-07-23 19:44:03 +0800

    feat: 样式提取到文件； 新的更细致的编号规则

--- html/js/indexer.js
+++ html/js/indexer.js
@@ -1,3 +1,4 @@
+'use strict';
 // 根据检测，li元素里面会有的子元素有UL,A,P,EM,CODE,PRE
 const kskb = 'KSKB';
 const list = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'TABLE', 'P', 'LI', kskb]; //  'A', 'P',
@@ -7,17 +8,39 @@ const indexes = [];
 const zhmap = new Map();
 let index = 0;
 document.querySelectorAll(`#page-core-content ${list.join(',')}`).forEach((el) => {
-  const ul = el.querySelector(`UL`);
-  if (ul === null) {
+  index++;
+  const ls = el.querySelectorAll(`UL,OL`);
+  const ps = el.querySelectorAll(`P`);
+
+  // 子元素中有列表的li元素
+  if (ls.length > 0 && el.tagName === 'LI') {
+    const otherEls = Array.from(el.childNodes).filter(
+      (child) => child.tagName !== 'UL' && child.tagName !== 'OL'
+    );
+    const kskbWrapper = document.createElement(kskb);
+    kskbWrapper.append(...otherEls);
+    kskbWrapper.setAttribute('data-en', index);
+    kskbWrapper.style.display = 'block';
+    el.insertBefore(kskbWrapper, ls[0]);
+    return;
+  }
+
+  // 子元素中有p元素的li元素
+  if (ls.length === 0 && ps.length > 0 && el.tagName === 'LI') {
+    const otherEls = Array.from(el.childNodes).filter((child) => child.tagName !== 'P');
+    const kskbWrapper = document.createElement(kskb);
+    kskbWrapper.append(...otherEls);
+    kskbWrapper.setAttribute('data-en', index);
+    kskbWrapper.style.display = 'block';
+    el.insertBefore(kskbWrapper, ps[0]);
+    return;
+  }
+
+  // 没有子列表的
+  if (ls.length === 0) {
     el.setAttribute('data-en', index);
     zhmap.set(index, el);
     indexes.push(index);
-  } else if (el.tagName === 'LI') {
-    const nonSubEls = Array.from(el.childNodes).filter((child) => child.tagName !== 'UL');
-    const kskbWrapper = document.createElement(kskb);
-    kskbWrapper.append(...nonSubEls);
-    el.insertBefore(kskbWrapper, ul);
-    kskbWrapper.setAttribute('data-en', index);
+    return;
   }
-  index++;
 });
